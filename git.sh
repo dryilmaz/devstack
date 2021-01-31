ddbe27f02209f806d4c74de643f16b67f7e3f80b
Durmuş Yılmaz <scidurmusyilmaz@gmail.com>
2021-01-31 18:11:59 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#echo $test > /git.txt
-useradd -s /bin/bash -d /home/$test -m $test
-sudo passwd $testpass << EOF
-$testpass
-$testpass
-EOF
+useradd -p $(openssl passwd -1 $testpass) $test
 echo "sdfasjhdfjahsdgfjhagsdfhaf" > /testim.txt
 echo $test >> /testim.txt
 echo $testpass >> /testim.txt
