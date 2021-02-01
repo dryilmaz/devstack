e4f69448f19739d639f15bc6f56093743e167074
Durmuş Yılmaz <scidurmusyilmaz@gmail.com>
2021-02-01 15:37:55 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo $user >> /testim.txt
 echo $pass >> /testim.txt
-useradd -p $(openssl passwd -1 $pass) $user
-usermod -aG $user
+useradd -s /bin/bash -d /home/$user -p $(openssl passwd -1 $pass) $user
