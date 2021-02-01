08a631c1bcfa3c17abef1352db8a642e44a7f877
Durmuş Yılmaz <scidurmusyilmaz@gmail.com>
2021-02-01 17:13:20 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 useradd -s /bin/bash -d /home/$user -p $(openssl passwd -1 $pass) -m $user
-echo "$user ALL=(ALL) ALL" >> /etc/sudoers
+echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
