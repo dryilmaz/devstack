dfab468db1fd90c31d6ff8acb18b5f622949d717
Durmuş Yılmaz <scidurmusyilmaz@gmail.com>
2021-02-01 16:23:45 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@
 echo $user >> /testim.txt
 echo $pass >> /testim.txt
 mkdir /home/$user
-useradd -s /bin/bash -d /home/$user -p $(openssl passwd -1 $pass) $user
+useradd -s /bin/bash -d /home/$user -p $(openssl passwd -1 $pass) -m $user
 chown $user:$user /home/$user
 echo "$user ALL=(ALL) ALL" >> /etc/sudoers
